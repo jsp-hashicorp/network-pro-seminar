6ec7ed58f96a0d93fedaa3426e30e46207741800
JS Park <jsp@hashicorp.com>
2020-11-04 00:43:54 +0900

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -6,8 +6,7 @@ local_ipv4="$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)"
 #Utils
 sudo apt-get install unzip
 
-#Install Dockers
-sudo snap install docker
+
 
 #Download Consul
 CONSUL_VERSION="1.7.2"
@@ -88,7 +87,21 @@ sudo service consul start
 sudo service consul status
 
 #Install Dockers
-#sudo snap install docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
