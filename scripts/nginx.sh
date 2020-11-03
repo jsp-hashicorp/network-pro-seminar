15b5090f54b0a85ca1b24d20b6781ed34042fa2f
JS Park <jsp@hashicorp.com>
2020-11-04 00:59:29 +0900

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -87,35 +87,5 @@ sudo service consul start
 sudo service consul status
 
 #Install Dockers
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-#Run  nginx
-sleep 10
-cat << EOF > docker-compose.yml
-version: "3.7"
-services:
-  web:
-    image: nginxdemos/hello
-    ports:
-    - "80:80"
-    restart: always
-    command: [nginx-debug, '-g', 'daemon off;']
-    network_mode: "host"
-EOF
-sudo docker-compose up -d
+sudo snap install docker
+sudo docker run -P -d nginxdemos/hello
