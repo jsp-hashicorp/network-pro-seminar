ff175e605b76daa7402d1f80f115b790fa2eeb79
JS Park <jsp@hashicorp.com>
2020-11-04 00:05:31 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-provider "consul" {
-  address    = "http://${aws_instance.consul.public_ip}:8500"
-  datacenter = var.consul_dc
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
