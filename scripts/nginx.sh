b435f1ca64ebeed9b43bf47795a1a06602d59991
JS Park <jsp@hashicorp.com>
2020-11-04 01:27:41 +0900

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -87,17 +87,6 @@ sudo service consul start
 sudo service consul status
 
 #Install Dockers
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install docker-ce
-sudo usermod -aG docker ${USER}
-sudo docker run -P -d nginxdemos/hello
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 sudo docker run -P -d nginxdemos/hello
